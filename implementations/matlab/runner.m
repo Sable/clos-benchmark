fb1c36f09347edd0f84d0afb6a281e9e392f07be
Erick <github@ericklavoie.com>
2016-09-09 17:56:44 +0200

    Added matlab implementation

---
+++ implementations/matlab/runner.m
@@ -0,0 +1,22 @@
+function runner(scale)
+%%
+%% Driver for the transitive closure of a directed graph.
+%%
+
+N=450;
+
+tic();
+for time=1:scale
+  B=closure(N);
+end
+elapsed = toc();
+
+disp('{');
+disp('"time":');
+disp(elapsed);
+disp(', "output":');
+disp(fletcherSum(B));
+disp('}');
+
+end
+
